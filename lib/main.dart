56db9113f36496db9f60352af874622d15ffd775
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-11 20:48:16 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Instagram'),
+        backgroundColor: Colors.black38,
+        centerTitle: false,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+
+      )
+    ),
+  ));
+}
+
+
