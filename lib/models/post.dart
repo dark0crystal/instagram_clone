d04d77e0b0a4917c542e5c602a7e542ebb1b6d7e
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-11 21:17:03 +0400

    add constructor

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -7,4 +7,14 @@ class Post {
   int? likes;
   int? shares;
   int? comments;
+
+
+  Post({
+    required this.userName,
+    required this.description,
+    required this.image,
+    required this.likes,
+    required this.shares,
+    required this.comments
+  });
 }
