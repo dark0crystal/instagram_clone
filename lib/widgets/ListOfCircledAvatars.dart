b47a3b15efa27fd0040c9cd5c09b486435fcb27c
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-12 09:06:18 +0400

    create a scrollable avatars , using ListView

---
+++ lib/widgets/ListOfCircledAvatars.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/data/AvatarsData.dart';
+
+class Listofcircledavatars extends StatefulWidget {
+  const Listofcircledavatars({super.key});
+
+  @override
+  State<Listofcircledavatars> createState() => _ListofcircledavatarsState();
+}
+
+class _ListofcircledavatarsState extends State<Listofcircledavatars> {
+  @override
+  Widget build(BuildContext context) {
+    return
+    Container(
+      child:Row(
+        children:[
+          Expanded(
+          child:ListView.builder(
+            scrollDirection:Axis.horizontal,
+            itemCount: Avatars.length,
+            itemBuilder: (_ , index) {
+              final avatar = Avatars[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child:Column(
+                  children:[
+                   CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage(avatar.image),
+                   ),
+                   Text(avatar.userName),
+                  ],
+                   
+                ),
+              );
+            },
+
+          ),
+          ),
+      ],
+      ) 
+    );
+    
+  }
+}
