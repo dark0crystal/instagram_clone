3b55df39c7838a4635a5c08b90ef6fba9615d241
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-12 14:54:21 +0400

    change post data

--- lib/data/PostsData.dart
+++ lib/data/PostsData.dart
@@ -5,7 +5,7 @@ List<Post> Posts =[
   Post(
    userName: "user1",
    description: "good post",
-   image: "lib/assets/alqurm.jpeg",
+   image: "lib/assets/img1.jpeg",
    likes: 3,
    shares: 4, 
    comments: 44,
@@ -13,7 +13,7 @@ List<Post> Posts =[
   Post(
    userName: "user2",
    description: "good posts is here",
-   image: "lib/assets/alqurm.jpeg",
+   image: "lib/assets/img2.jpeg",
    likes: 23,
    shares: 34, 
    comments: 64,
@@ -21,7 +21,7 @@ List<Post> Posts =[
    Post(
    userName: "user3",
    description: "posts is here",
-   image: "lib/assets/alqurm.jpeg",
+   image: "lib/assets/img6.jpeg",
    likes: 23,
    shares: 34, 
    comments: 64,
@@ -29,7 +29,7 @@ List<Post> Posts =[
    Post(
    userName: "user4",
    description: "Oman is a good place to visit",
-   image: "lib/assets/alqurm.jpeg",
+   image: "lib/assets/img4.jpeg",
    likes: 2,
    shares: 333, 
    comments: 6,
