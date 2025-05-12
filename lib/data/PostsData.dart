12bfde80b594225f399b57745ba16420077f30b8
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-12 11:33:05 +0400

    post data

---
+++ lib/data/PostsData.dart
@@ -0,0 +1,37 @@
+
+import 'package:instagram_clone/models/post.dart';
+
+List<Post> Posts =[
+  Post(
+   userName: "user1",
+   description: "good post",
+   image: "lib/assets/alqurm.jpeg",
+   likes: 3,
+   shares: 4, 
+   comments: 44,
+   ),
+  Post(
+   userName: "user2",
+   description: "good posts is here",
+   image: "lib/assets/alqurm.jpeg",
+   likes: 23,
+   shares: 34, 
+   comments: 64,
+   ),
+   Post(
+   userName: "user3",
+   description: "posts is here",
+   image: "lib/assets/alqurm.jpeg",
+   likes: 23,
+   shares: 34, 
+   comments: 64,
+   ),
+   Post(
+   userName: "user4",
+   description: "Oman is a good place to visit",
+   image: "lib/assets/alqurm.jpeg",
+   likes: 2,
+   shares: 333, 
+   comments: 6,
+   ),
+];
