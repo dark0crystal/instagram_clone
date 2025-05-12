ccecfd972cf023231d61c31ffddb47eefa24ac07
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-12 14:54:49 +0400

    add more content

--- lib/widgets/main_page_posts.dart
+++ lib/widgets/main_page_posts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/data/PostsData.dart';
+
 class MainPagePosts extends StatefulWidget {
   const MainPagePosts({super.key});
 
@@ -10,43 +11,44 @@ class MainPagePosts extends StatefulWidget {
 class _MainPagePostsState extends State<MainPagePosts> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Expanded(
-            
-            child:ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: Posts.length,
-              itemBuilder: (_, index){
-                final posts = Posts[index];
-                return Container(
-                  padding :EdgeInsets.all(4),
-                  margin: EdgeInsets.all(4),
-                  child:Expanded(
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            TextButton(onPressed: null, child: Text(posts.userName))
-                          ],
-                        ),
-                        Column(
-                          children: [
-
-                          ],
-                        )
-                      ],
-                  ),
-                  ),
-                );
+    return ListView.builder(
+      itemCount: Posts.length,
+      itemBuilder: (_, index) {
+        final posts = Posts[index];
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 
-              } ,
-            )
-            
-          )
-        ],
-      ),
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage(posts.image),
+                        radius: 20,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(posts.userName),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Follow"),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Image.asset(posts.image),
+              Row(children: [
+                Text(posts.description)
+              ],)
+            ],
+          ),
+        );
+      },
     );
   }
 }
