48f0893a21971ca6cd994c227e06b6516287bfaa
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-12 09:08:57 +0400

    add avatars fake data

---
+++ lib/data/AvatarsData.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:instagram_clone/models/avatar.dart';
+
+List <Avatar> Avatars =[
+  Avatar(
+    userName:"toto" , 
+    image:  'lib/assets/img1.jpeg',
+  ),
+  Avatar(
+    userName:"roro" , 
+    image:  'lib/assets/img2.jpeg',
+  ),
+  Avatar(
+    userName:"gogo" , 
+    image:  'lib/assets/img3.jpeg',
+  ),
+  Avatar(
+    userName:"bobo" , 
+    image:  'lib/assets/img4.jpeg',
+  ),
+  Avatar(
+    userName:"eoeo" , 
+    image:  'lib/assets/img5.jpeg',
+  ),
+];
