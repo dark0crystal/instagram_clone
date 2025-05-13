83356059df19c5dff41064e1634ff16f9555c64d
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-13 07:47:16 +0400

    use the parameter in the profile

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/post.dart';
 
 class Profile extends StatefulWidget {
-  const Profile({super.key});
+  const Profile({super.key ,
+  required this.post,
+  });
 
+  final Post post;
   @override
   State<Profile> createState() => _ProfileState();
 }
@@ -13,6 +17,15 @@ class _ProfileState extends State<Profile> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Row(children: [
+              Text(widget.post.userName),
+            ],)
+          ],
+        ),
       ),
     );
   }
