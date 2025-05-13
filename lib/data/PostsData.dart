1f5d1421c8151b9754acfa08ab5aed0ad1e1ebb3
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-13 07:46:52 +0400

    add id to each post

--- lib/data/PostsData.dart
+++ lib/data/PostsData.dart
@@ -3,6 +3,7 @@ import 'package:instagram_clone/models/post.dart';
 
 List<Post> Posts =[
   Post(
+   id: 1, 
    userName: "user1",
    description: "good post",
    image: "lib/assets/img1.jpeg",
@@ -11,6 +12,7 @@ List<Post> Posts =[
    comments: 44,
    ),
   Post(
+    id:2,
    userName: "user2",
    description: "good posts is here",
    image: "lib/assets/img2.jpeg",
@@ -19,6 +21,7 @@ List<Post> Posts =[
    comments: 64,
    ),
    Post(
+    id:3,
    userName: "user3",
    description: "posts is here",
    image: "lib/assets/img6.jpeg",
@@ -27,6 +30,7 @@ List<Post> Posts =[
    comments: 64,
    ),
    Post(
+    id:4,
    userName: "user4",
    description: "Oman is a good place to visit",
    image: "lib/assets/img4.jpeg",
